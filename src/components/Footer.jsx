2e8f1d90a1cfa1d6d12d1c518180857564d252cb
Sharan Panthi <shpanthi09@gmail.com>
2024-12-02 20:05:38 +0545

    home page update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,7 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <div className="footer-logo">
-          <h2>Sharan Panthi</h2>
-        </div>
+        <div className="footer-logo">{/* <h4>Sharan Panthi</h4> */}</div>
         <div className="footer-social">
           <a
             href="https://github.com/codes-sharan"
@@ -44,3 +42,44 @@ function Footer() {
 }
 
 export default Footer;
+
+// function Footer() {
+//   return (
+//     <footer className="footer">
+//       <div className="footer-content">
+//         <div className="footer-logo">{/* <h4>Sharan Panthi</h4> */}</div>
+//         <div className="footer-social">
+//           <a
+//             href="https://github.com/codes-sharan"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//             className="footer-icon"
+//           >
+//             <FaGithub />
+//           </a>
+//           <a
+//             href="https://linkedin.com/in/sharan-panthi-94765382/"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//             className="footer-icon"
+//           >
+//             <FaLinkedin />
+//           </a>
+//           <a
+//             href="https://twitter.com/PanthiSharan"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//             className="footer-icon"
+//           >
+//             <FaX />
+//           </a>
+//         </div>
+//       </div>
+//       <div className="footer-bottom">
+//         <p>&copy; 2024 Sharan Panthi. All Rights Reserved.</p>
+//       </div>
+//     </footer>
+//   );
+// }
+
+// export default Footer;
